ad5afc8427f210b146d1d801a46bb854e1808817
NadiaParra <nadia.parra@live.u-tad.com>
2017-06-05 20:53:37 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+jQuery(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#262626', '#999999', '#262626', '#999999', '#262626'],
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		menu: '#menu',
+		scrollingSpeed: 1000
+	});
+});
+
+
+
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {
+	jQueryfds.fn.fullpage();
+});
