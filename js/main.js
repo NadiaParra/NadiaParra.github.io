df50a806c85df96e1e9277ad44b66796d519bd4e
NadiaParra <nadia.parra@live.u-tad.com>
2017-06-09 10:44:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function() {
 	$('#fullpage').fullpage({
 		sectionsColor: ['#262626', '#999999', '#262626', '#999999', '#262626'],
-		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage'],
 		menu: '#menu',
 		scrollingSpeed: 1000
 	});
